d8eb551cb049bd924af0cc2e89c26d0d91476aa7
LeeMunHyung <mun0659@gmail.com>
2022-08-12 14:04:54 +0900

    probLayer

--- layers/attn.py
+++ layers/attn.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import numpy as np
-from math import sqrt
+from math import factorial, sqrt
 
-from utils.mask import TriangularCausalMask
+from utils.mask import TriangularCausalMask,ProbMask
 
 class FullAttention(nn.Module):
     def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
@@ -37,6 +37,92 @@ class FullAttention(nn.Module):
         
         else:
             return (V.contiguous(),None)
+
+class ProbAttention(nn.Module):
+    def __init__(self, mask_flag=True, factor=5,scale=None, attention_dropout = 0.1, output_attention=False):
+        super(ProbAttention, self).__init__()
+        self.factor = factor
+        self.scale = scale
+        self.mask_flag = mask_flag
+        self.output_attention = output_attention
+        self.dropout = nn.Dropout(attention_dropout)
+    
+
+    def _prob_QK(self, Q, K, sample_k, n_top):
+        #n_top: c*ln(L_q)
+        #Q [B,H,L,D]
+        B,H,L_K, E = K.shape
+        _,_,L_Q,_ = Q.shape
+
+        #calculate the smaple Q_k
+        K_expand = K.unsqueeze(-3).expand(B,H,L_Q,L_K,E)
+        index_sample = torch.randint(L_K,(L_Q,sample_k))
+        K_sample = K_expand[:,:,torch.arange(L_Q).unsqueeze(1), index_sample,:]
+        Q_K_sample = torch.matmul(Q.squeeze(-2),K_sample.transpose(-2,-1)).squeeze(-2)
+
+        # find the Top_k query with sparisty measurement
+        M = Q_K_sample.max(-1)[0] - torch.div(Q_K_sample.sum(-1), L_K)
+        M_top = M.topk(n_top,sorted=False)[1]
+
+        Q_reduce = Q[torch.arange(B)[:,None, None],
+                    torch.arange(H)[None, :, None]]
+        
+        Q_K = torch.matmul(Q_reduce, K.transpose(-2,-1))
+
+        return Q_K, M_top
+    
+
+    def _get_initial_context(self, V, L_Q):
+        B, H, L_V, D =V.shape
+        if not self.mask_flag:
+            V_sum = V.mean(dim=-2)
+            contex = V_sum.unsqueeze(-2).expand(B, H, L_Q, V_sum.shape[-1]).clone()
+        else:
+            assert(L_Q == L_V)
+            contex = V.cumsum(dim=-2)
+        return contex
+
+    def _update_context(self, context_in, V, scores, index , L_Q, attn_mask):
+        B, H, L_V, D = V.shape
+
+        if self. mask_falg:
+            attn_mask = ProbMask(B,H,L_Q,index, scores, device = V.device)
+            scores.masked_fill_(attn_mask.mask, -np.inf)
+        
+        attn = torch.softmax(scores, dim=-1)
+
+        context_in[torch.arange(B)[:, None, None],
+                    torch.arange(H)[None, :, None],
+                    index, :] = torch.matmul(attn, V).type_as(context_in)
+        
+        if self.output_attention:
+            attns = (torch.ones([B, H, L_V, L_V])/L_V).type_as(attn).to(attn.device)
+            attns[torch.arange(B)[:, None, None], torch.arange(H)[None, :, None], index, :] = attn
+            return (context_in, attns)
+        else:
+            return (context_in, None)
+    
+    def forward(self, queries, keys, values, attn_mask):
+        B, L_Q, H, D = queries.shape
+        _, L_K, _, _ = keys.shape
+
+        queries = queries.transpose(2,1)
+        keys = keys.transpose(2,1)
+        values = values.transpose(2,1)
+
+        U_part = self.factor * np.ceil(np.log(L_K)).astype('int').item()
+        u = self.factor * np.ceil(np.log(L_Q)).astype('int').item()
+
+        scores_top, index = self._prob_QK(queries, keys, sample_k = U_part, n_top=u)
+
+        scale = self.scale or 1./sqrt(D)
+        if scale is not None:
+            scores_top = scores_top * scale
+            context = self._get_initial_context(values, L_Q)
+            context, attn = self._update_context(context, values, scores_top,index, L_Q,attn_mask)
+
+            return context.tranpose(2,1).contiguous(), attn
+
 
 
 class AttentionLayer(nn.Module):
